d4e4d334a11013a9e65e468072ff33b457f63ba6
yogesh1612 <yogesh1612@users.noreply.github.com>
2021-04-12 15:53:53 +0530

    Create dependencies.R

---
+++ dependencies.R
@@ -0,0 +1,10 @@
+
+if(!require(devtools)) {install.packages("devtools")};library("devtools")
+if(!require(spotifyr)) {devtools::install_github('charlie86/spotifyr')};library("spotifyr")
+if(!require(tidyverse)) {install.packages("tidyverse")};library("tidyverse")
+if(!require(shiny)) {install.packages("shiny")};library("shiny")
+if(!require(shinyWidgets)) {install.packages("shinyWidgets")};library("shinyWidgets")
+if(!require(httr)) {install.packages("httr")};library("httr")
+if(!require(DT)) {install.packages("DT")};library("DT")
+if(!require(markdown)) {install.packages("markdown")};library("markdown")
+
